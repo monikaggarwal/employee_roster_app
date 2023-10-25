b16aeec937d31fef188f9957b90d7f190a4fd7cc
monikaggarwal <68156957+monikaggarwal@users.noreply.github.com>
2023-10-25 22:26:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import EmployeeModal from "./Components/EmployeeModal"; // Import EmployeeModal
+import EmployeeModal from "./Components/EmployeeModal";
 
 function App() {
   const [employeeData, setEmployeeData] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
-  const [selectedEmployee, setSelectedEmployee] = useState(null); // State to store selected employee
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedEmployee, setSelectedEmployee] = useState(null); 
   const employeesPerPage = 10;
 
   useEffect(() => {
