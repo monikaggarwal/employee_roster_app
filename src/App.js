0c44edd239888eace70e0a3a49fde8d68d4152f5
Monika Aggarwal <monika.aggarwal@ad.infosys.com>
2023-10-26 13:10:36 +0530

    addded search button and stylings for table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import EmployeeModal from "./Components/EmployeeModal"; 
+import EmployeeModal from "./Components/EmployeeModal";
 
 function App() {
   const [employeeData, setEmployeeData] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
   const employeesPerPage = 10;
 
   useEffect(() => {
@@ -44,11 +45,24 @@ function App() {
     setSelectedEmployee(null);
   };
 
-  const getPaginatedEmployees = () => {
-    if (employeeData && employeeData.length > 0) {
+  const handleSearch = () => {
+    console.log("Search Query: ", searchQuery);
+  };
+
+  const filteredEmployees = () => {
+    return (
+      employeeData &&
+      employeeData.filter((employee) =>
+        employee.empName.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  };
+
+  const paginatedEmployees = () => {
+    if (filteredEmployees() && filteredEmployees().length > 0) {
       const startIndex = (currentPage - 1) * employeesPerPage;
       const endIndex = startIndex + employeesPerPage;
-      return employeeData.slice(startIndex, endIndex);
+      return filteredEmployees().slice(startIndex, endIndex);
     } else {
       return [];
     }
@@ -56,8 +70,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Infosys Ltd.</h1>
-      <table>
+      <h1>
+        Infosys Ltd.
+      </h1>
+      <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button> 
+        </div>
+      <table class="rosterTable">
         <thead>
           <tr>
             <th>ID</th>
@@ -67,14 +92,23 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {getPaginatedEmployees().map((employee) => (
-            <tr key={employee.id} onClick={() => openModal(employee)}>
-              <td>{employee.id}</td>
-              <td><img src={"/" + employee.empPhoto} alt={employee.name} height="30" width="30"></img>{employee.empName}</td>
-              <td>{employee.contactNo}</td>
-              <td>{employee.address}</td>
-            </tr>
-          ))}
+          {paginatedEmployees() &&
+            paginatedEmployees().map((employee) => (
+              <tr key={employee.id} onClick={() => openModal(employee)}>
+                <td>{employee.id}</td>
+                <td>
+                  <img
+                    src={"/" + employee.empPhoto}
+                    alt={employee.empName}
+                    height="30"
+                    width="30"
+                  />
+                  {employee.empName}
+                </td>
+                <td>{employee.contactNo}</td>
+                <td>{employee.address}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
       <div className="pagination">
@@ -85,11 +119,15 @@ function App() {
           Previous
         </button>
         <span>
-          Page {currentPage} of {Math.ceil((employeeData?.length || 0) / employeesPerPage)}
+          Page {currentPage} of{" "}
+          {Math.ceil((filteredEmployees()?.length || 0) / employeesPerPage)}
         </span>
         <button
           onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage * employeesPerPage >= (employeeData?.length || 0)}
+          disabled={
+            currentPage * employeesPerPage >=
+            (filteredEmployees()?.length || 0)
+          }
         >
           Next
         </button>
